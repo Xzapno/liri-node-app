703bf28708593944fd3602f52a0a73f3c5df9136
Paul <hoffsbox@gmail.com>
2018-09-03 14:18:52 -0400

    Updates

--- liri.js
+++ liri.js
@@ -8,6 +8,9 @@ var spotify = require("spotify")
 var input = process.argv[2];
 var input2 = process.argv[3];
 
+//var spotify = new spotify(keys.spotify);
+var client = new twitter(keys.twitter);
+
 
 // Take in command from terminal
 //    * `my-tweets`
@@ -19,10 +22,10 @@ switch(input) {
     case "my-tweets": tweets();
     break;
 
-    case "spotify-this-song": spotifySong();
+    case "spotify-this-song": spotifySong(input2);
     break;
 
-    case "movie-this": getMovie(input2);
+    case "movie-this": getMovie();
     break;
 
     case "do-what-it-says": doIt();
@@ -30,58 +33,65 @@ switch(input) {
 }
 
 
-
 // Twitter API function
 
-function tweets() {
-
-    var client = new Twitter({
-        consumer_key: '',
-        consumer_secret: '',
-        access_token_key: '',
-        access_token_secret: ''
-      });
-       
-      var params = {screen_name: 'nodejs'};
-      client.get('statuses/user_timeline', params, function(error, tweets, response) {
-        if (!error) {
-          console.log(tweets);
-        }
-      });
-}
+    // function tweets() {
+    //     var params = {screen_name: 'HoffHerr'};
+    //     client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    //         if (!error) {
+    //         console.log(tweets);
+    //         for(var i = 0; i < tweets.length; i++) {
+    //             console.log(tweets[i].text);
+    //             console.log("--------------------------");
+    //         }
+    //         }
+    //     });
+    // }
 
 
 // Spotify API fumction
-
-function spotifySong() {
-
-}
+    
+    var spotify = new Spotify({
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+    });
+    
+    function spotifySong(input2) {
+        spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            }
+           
+          console.log(data); 
+          });
+    } 
+    
 
 
 // OMDB API Function
 
-function getMovie() {
+// function getMovie() {
 
-// Include the request npm package (Don't forget to run "npm install request" in this folder first!)
-    var request = require("request");
+// // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
+//     var request = require("request");
 
-// Then run a request to the OMDB API with the movie specified
-    request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
+// // Then run a request to the OMDB API with the movie specified
+//     request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
 
-// If the request is successful (i.e. if the response status code is 200)
-    if (!error && response.statusCode === 200) {
+// // If the request is successful (i.e. if the response status code is 200)
+//     if (!error && response.statusCode === 200) {
 
-// Parse the body of the site and recover just the imdbRating
-// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-    }
-});
+// // Parse the body of the site and recover just the imdbRating
+// // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+//     console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+//     }
+// });
 
 
-}
+// }
 
-// Do What it Says Function
+// // Do What it Says Function
 
-function doIt(){
+// function doIt(){
 
-}
+// }
